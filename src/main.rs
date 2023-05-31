9c9e465f51f29edc7f01359c3610603a004bc282
Mikhail Kabanov <mike.k@didlogic.com>
2023-05-31 18:50:11 +0600

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,16 @@
 use crate::command::on_receive_command;
 use crate::db::DB;
 use crate::utils::*;
-use db::User;
 use dotenv::dotenv;
 use log::LevelFilter;
 
 use std::sync::{Arc, Mutex};
 use teloxide::{prelude::*, Bot};
-use tokio_interval::{clear_timer, set_interval};
 
 mod command;
 mod db;
 mod gpt;
 mod utils;
-
-async fn on_receive_message(state_users: Vec<User>, bot: Bot, msg: Message) {
-    let user_request = find_user_by_username(&state_users, msg.chat.username().unwrap());
-    let bot_cloned = bot.clone();
-
-    if let Some(user) = user_request {
-        let is_voice_response_required = is_tts_enabled(&user);
-
-        let typing_interval = set_interval!(
-            move || {
-                if is_voice_response_required {
-                    tokio::spawn(send_voice_recording_action(bot_cloned.clone(), msg.chat.id));
-                } else {
-                    tokio::spawn(send_typing_action(bot_cloned.clone(), msg.chat.id));
-                }
-            },
-            3000
-        );
-
-        proccess_message(user.clone(), bot, msg).await;
-        clear_timer!(typing_interval)
-    } else {
-        send_message(bot, msg.chat.id, "Access denied").await;
-    }
-}
-
-async fn proccess_message(user: User, bot: Bot, msg: Message) {
-    let voice = msg.voice();
-    let message = msg.text();
-
-    let mut content = "";
-
-    if voice.is_some() {
-        content = asr(bot.clone(), &voice.unwrap().file).await;
-    }
-
-    if message.is_some() {
-        content = msg.text().unwrap();
-    }
-
-    if content.trim().is_empty() {
-        return;
-    }
-
-    proccess_text_message(TextMessage {
-        user: user,
-        bot: bot,
-        chat_id: msg.chat.id,
-        message: content.clone(),
-    })
-    .await;
-}
 
 fn init_sentry() {
     let _guard = sentry::init((
--- src/utils.rs
+++ src/utils.rs
@@ -13,6 +13,7 @@ use teloxide::{
     types::{ChatAction, FileMeta},
 };
 use tokio::fs::OpenOptions;
+use tokio_interval::{clear_timer, set_interval};
 
 #[derive(Debug)]
 pub struct State {
@@ -214,4 +215,56 @@ pub fn is_code_listing(text: &str) -> bool {
     }
 
     false
+}
+
+pub async fn proccess_message(user: User, bot: Bot, msg: Message) {
+    let voice = msg.voice();
+    let message = msg.text();
+
+    let mut content = "";
+
+    if voice.is_some() {
+        content = asr(bot.clone(), &voice.unwrap().file).await;
+    }
+
+    if message.is_some() {
+        content = msg.text().unwrap();
+    }
+
+    if content.trim().is_empty() {
+        return;
+    }
+
+    proccess_text_message(TextMessage {
+        user: user,
+        bot: bot,
+        chat_id: msg.chat.id,
+        message: content.clone(),
+    })
+    .await;
+}
+
+pub async fn on_receive_message(state_users: Vec<User>, bot: Bot, msg: Message) {
+    let user_request = find_user_by_username(&state_users, msg.chat.username().unwrap());
+    let bot_cloned = bot.clone();
+
+    if let Some(user) = user_request {
+        let is_voice_response_required = is_tts_enabled(&user);
+
+        let typing_interval = set_interval!(
+            move || {
+                if is_voice_response_required {
+                    tokio::spawn(send_voice_recording_action(bot_cloned.clone(), msg.chat.id));
+                } else {
+                    tokio::spawn(send_typing_action(bot_cloned.clone(), msg.chat.id));
+                }
+            },
+            3000
+        );
+
+        proccess_message(user.clone(), bot, msg).await;
+        clear_timer!(typing_interval)
+    } else {
+        send_message(bot, msg.chat.id, "Access denied").await;
+    }
 }
